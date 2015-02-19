97dbeb2a32cca682defbbabdaa877659a295f4fc
Bogdan Vitoc <bogdanvitoc@gmail.com>
2015-02-18 21:43:43 -0500

    GitPull fix

--- routes/serverUtils.js
+++ routes/serverUtils.js
@@ -4,22 +4,23 @@ var gitPull = {
 	},
 	post: function (req, res) {
 		console.log("Pulling...");
-		run_cmd("git stash", function() {
-			run_cmd("git pull origin master", function() {
-				run_cmd("git stash apply");		
-			});	
-		});
+		run_cmd("git pull");
+		// run_cmd("git stash", function() {
+		// 	run_cmd("git pull origin master", function() {
+		// 		run_cmd("git stash apply");		
+		// 	});	
+		// });
 		
 		console.log("Pulled!");
 		res.send("Pulled!", 200);
 	}
 }
 
-function run_cmd(cmd, callback) {
+function run_cmd(cmd) {
     var sys = require('sys')
 	var exec = require('child_process').exec;
 	function puts(error, stdout, stderr) { sys.puts(stdout) }
-	exec(cmd, puts, callback);
+	exec(cmd, puts);
 }
 
 exports.gitPull = gitPull;
