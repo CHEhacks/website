84760eaa5d1d01bb66d7fd379790e937c5ee180b
Bogdan Vitoc <bogdanvitoc@gmail.com>
2015-09-15 00:07:55 -0400

    Updated resume upload link

--- routes/resume.js
+++ routes/resume.js
@@ -1,3 +1,3 @@
 exports.get =  function (req, res) {
-	res.redirect('https://script.google.com/a/macros/chehacks.com/s/AKfycbxwlEeyYDahc1AAkq-JG6YEvVMYCNpjIBZIvaolj1M/dev');
+	res.redirect('https://script.google.com/macros/s/AKfycbx6r9dPkA8LflyeTbcP_57Ctbp835WddGf8OhEK9Quj8J-ZBLI/exec');
 };
